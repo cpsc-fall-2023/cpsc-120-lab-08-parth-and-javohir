b40f5462d7c9c176978b981a7b5a023963d7ea57
Parth Kheni <142745840+pkheni123@users.noreply.github.com>
2023-11-01 21:58:43 -0700

    Update average.cc

--- part-2/average.cc
+++ part-2/average.cc
@@ -6,31 +6,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " <value1> <value2> <value3> ..." << std::endl;
-        return 1;
+  std::vector<std::string> arguments{argv, argv + argc};
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one number\n";
+    return 1;
+  }
+  bool first{true};
+  double total{0.0};
+  for (std::string& argument : arguments) {
+    if (first) {
+      first = false;
+      continue;
     }
-
-    double sum = 0.0;
-    int numValues = argc - 1;
-
-    for (int i = 1; i < argc; i++) {
-        double value;
-        std::istringstream iss(argv[i]);
-
-        if (!(iss >> value)) {
-            std::cerr << "Invalid input: " << argv[i] << std::endl;
-            return 1;
-        }
-
-        sum += value;
-    }
-
-    double average = sum / numValues;
-    std::cout << "Average: " << average << std::endl;
-
-    return 0;
+    double num{std::stod(argument)};
+    total = total +  num;
+  }
+  double denom{static_cast<double>(arguments.size() - 1)};
+  double avg = total / denom;
+  std::cout << "average = " << avg;
+  return 0;
 }
